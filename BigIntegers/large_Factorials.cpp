d7760fa2e467b76d07becc5481d198d019c3b977
harryNeo9 <harishankarpv9@gmail.com>
2022-06-23 19:25:13 +0530

    big integer multiplication & larger factorial

--- BigIntegers/large_Factorials.cpp
+++ BigIntegers/large_Factorials.cpp
@@ -23,25 +23,50 @@
     #define loop(i, a, b) for (int i = (a); i <= (b); i++)
     #define looprev(i, a, b) for (int i = (a); i >= (b); i--)
 
+    void  bigIntegerMultiplicaion(vector<int>& a, int num, int& size){
+        int carry = 0;
+
+        for(int i=0; i<size; i++){
+            int product = a[i] * num + carry;
+            a[i] = product % 10;
+            carry = product / 10;
+        }
+
+        while(carry){
+            a[size] = carry % 10;
+            carry = carry/10;
+            size += 1;
+        }
+    }
+
+    void factorial(int n){
+        //set size of factorial array as 1000
+        //size can be inferred from problem constraints
+        vector<int> a(1000, 0);
+
+        // Initialise 1! as 1
+        a[0] = 1;
+
+        //keeps track of the no of digits of factorial computed
+        int size_of_fac = 1;
+
+        for(int i=2; i<=n; i++){
+            bigIntegerMultiplicaion(a, i, size_of_fac);
+        }
+
+        //print the output in the reverse order
+        for(int i=size_of_fac-1; i>=0; i--){
+            cout<<a[i];
+        }
+        cout<<endl;
+    }
+
     int main(){
 
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        int t;
-        cin >> t;
-        while (t--){
-            int n;
-            cin >> n;
-            vector<int> v(n);
-            for (int i = 0; i < n; i++){
-                cin >> v[i];
-            }
-            for (auto x : v){
-                cout << x << " ";
-            }
-            cout << endl;
-        }
+       factorial(5);
 
         return 0;
     }
